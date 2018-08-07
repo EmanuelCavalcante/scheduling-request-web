695413ad6c5d2a63a47b41378fba61b8abffd302
Emanuel Julião Lima Cavalcante <emanuellima3105@gmail.com>
2018-08-07 18:32:04 -0300

    Created project

---
+++ src/main/java/com/taskrequestapi/TaskRequestApiApplication.java
@@ -0,0 +1,12 @@
+package com.taskrequestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskRequestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskRequestApiApplication.class, args);
+	}
+}
