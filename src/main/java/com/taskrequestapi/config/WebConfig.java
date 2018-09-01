0d4dc4f3a41e2484f1515b1cf3c082fe7380ea70
Rogerio da Silva Araujo <rogeriosilva60@gmail.com>
2018-09-01 18:26:03 -0300

    Thymeleaf initial settings.

---
+++ src/main/java/com/taskrequestapi/config/WebConfig.java
@@ -0,0 +1,57 @@
+package com.taskrequestapi.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.ISpringTemplateEngine;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+import com.taskrequestapi.controller.UserController;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackageClasses = {UserController.class})
+public class WebConfig implements WebMvcConfigurer, ApplicationContextAware{
+
+	private ApplicationContext applicationContext;
+	
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
+	}
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+		resolver.setTemplateEngine(templateEngine());
+		resolver.setCharacterEncoding("UTF-8");
+		return resolver;
+	} 
+	
+	@Bean
+	public ISpringTemplateEngine templateEngine() {
+		SpringTemplateEngine engine = new SpringTemplateEngine();
+		engine.setEnableSpringELCompiler(true);
+		engine.setTemplateResolver(iTemplateResolver());
+		return engine;
+	}
+	
+	private ITemplateResolver iTemplateResolver() {
+		SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+		resolver.setApplicationContext(applicationContext);
+		resolver.setPrefix("classpath:/templates/");
+		resolver.setSuffix(".html");
+		resolver.setTemplateMode(TemplateMode.HTML);
+		return resolver;
+	}
+}
---
+++ src/main/java/com/taskrequestapi/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.taskrequestapi.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.taskrequestapi.models.User;
+
+@Controller
+public class UserController {
+
+	
+	@RequestMapping("/user/new")
+	public String newUser() {
+		return "user/RegisterUser";
+	}
+	
+	@RequestMapping(value = "/user/new", method = RequestMethod.POST)
+	public String cadastrar(@Valid User cerv,BindingResult result, Model model,RedirectAttributes attributes) {
+		
+		if(result.hasErrors()) {
+			model.addAttribute("message", "Form error");
+			return "user/RegisterUser";
+		}
+		
+		attributes.addFlashAttribute("message", "Saved successfully!");
+		return "redirect:/user/new";
+	}
+}
