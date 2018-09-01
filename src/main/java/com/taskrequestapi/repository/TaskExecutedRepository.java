6684f6dedd470ef3c0042f568f4b4cc82a6fc13b
Emanuel Julião Lima Cavalcante <emanuellima3105@gmail.com>
2018-09-01 16:54:21 -0300

    format code

--- src/main/java/com/taskrequestapi/repository/TaskExecutedRepository.java
+++ src/main/java/com/taskrequestapi/repository/TaskExecutedRepository.java
@@ -12,7 +12,9 @@ import com.taskrequestapi.models.TaskExecuted;
 
 @Repository
 public interface TaskExecutedRepository extends JpaRepository<TaskExecuted, Integer> {
-	@Query("SELECT taskExecuted FROM TaskExecuted taskExecuted where taskExecuted.user.id = :userId AND taskExecuted.task.id =:taskId AND taskExecuted.date>=:dateBeginning and taskExecuted.date<=:dateEnd ")
+	@Query("SELECT taskExecuted FROM TaskExecuted taskExecuted where taskExecuted.user.id = :userId "
+			+ "AND taskExecuted.task.id =:taskId AND taskExecuted.date>=:dateBeginning "
+			+ "AND taskExecuted.date<=:dateEnd ")
 	List<TaskExecuted> tasksExecuted(@Param("userId") Integer userId, @Param("taskId") Integer taskId,
 			@Param("dateBeginning") Date dateBeginning, @Param("dateEnd") Date dateEnd);
 }
